c4bbdf109ef2934e7eb91d3625119bbf92893148
rdosrun <rdosrun@gmail.com>
2020-10-09 20:27:41 -0700

    added drop down menu

--- app/src/main/java/com/example/service_project/park_detail.java
+++ app/src/main/java/com/example/service_project/park_detail.java
@@ -2,6 +2,7 @@ package com.example.service_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.View;
@@ -20,16 +21,49 @@ public class park_detail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park_detail);
         ll = findViewById(R.id.park_d_ll);
-        ArrayList<Button> issues = new ArrayList<>();
-        //View Screen = findViewByI;
 
+        //used to free up UI thread
+        Runnable R = new Runnable(){
+            @Override
+            public void run() {
+                create();
+            }
+        };
+        Thread task1 = new Thread(R);
+        task1.start();
+        //end Threaded tasks
+    }
+
+    public void create(){
+        //Generateing layout
         for (int i =0;i<5;i++){
-           // issues.add(new Button(this));
-           // PopupMenu popup = new PopupMenu(this, Screen);
-           // MenuInflater inflater = popup.getMenuInflater();
-           // inflater.inflate(R.menu.menu_example, popup.getMenu());
-            //popup.show();
-            ll.addView(new Button(this));
+            //issues button
+            Button issue = new Button(this);
+            //drop down menu
+            LinearLayout content = new LinearLayout(this);
+            //drop down menu content
+            Button issue2 = new Button(this);
+            issue2.setWidth(5000);
+            content.addView(issue2);
+
+            //drop down menu action
+            issue.setOnClickListener(v1->{
+                if(content.getVisibility()==LinearLayout.GONE){
+                    content.setVisibility(LinearLayout.VISIBLE);
+                }else{
+                    content.setVisibility(LinearLayout.GONE);
+                }
+
+            });
+            content.setVisibility(LinearLayout.GONE);
+            //end drop down menu action
+
+            Runnable add_to_screen = ()->{
+                ll.addView(issue);
+                ll.addView(content);
+            };
+            runOnUiThread(add_to_screen);
+
         }
     }
 
